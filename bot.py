6e57f5d44346896c1619e48cc0d93157bcb16a90
Luiggi Sperandio <luiggisperandio@gmail.com>
2024-07-31 12:29:09 -0300

    Fixed repo

---
+++ bot.py
@@ -0,0 +1,62 @@
+import os
+from twitchio.ext import commands
+import text_archive
+from random import randint
+
+
+# set up the bot
+bot = commands.Bot(
+    token=os.environ['TMI_TOKEN'],
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+print('bot ontheliner')
+
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+
+@bot.event()
+async def event_message(ctx):
+    # 'Runs every time a message is sent in chat.'
+    print(ctx)
+
+    # make sure the bot ignores itself and the streamer
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+        return
+
+    await bot.handle_commands(ctx)
+
+    #Roleta Russa do funk
+    a=0
+    if randint(1, 30) == 15:
+        a=1
+        await ctx.channel.send('15 anos no Brasil e não fala um portugues sem sotaque')
+    if randint(1, 10) == 5 and a == 0:
+        await ctx.channel.send('Bota um funk ai pra nois Boludo')
+    a=0
+    if 'salve' in ctx.content.lower():
+        await ctx.channel.send(f"Salve, @{ctx.author.name}!")
+
+
+@bot.command(name='shrek')
+async def test(ctx):
+    await ctx.send(text_archive.a)
+    await ctx.send(text_archive.b)
+    await ctx.send(text_archive.c)
+    await ctx.send(text_archive.d)
+    await ctx.send(text_archive.e)
+    await ctx.send(text_archive.f)
+    await ctx.send(text_archive.g)
+
+
+if __name__ == "__main__":
+    bot.run()
