844450f11d7149a4b4044d8bb98feea9ad0dc8d5
Luiggi Sperandio <luiggisperandio@gmail.com>
2024-07-31 21:29:47 -0300

    Added: people check

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 from twitchio.ext import commands
 import text_archive
 from random import randint
+import re
 
 
 # set up the bot
@@ -27,6 +28,8 @@ async def event_ready():
 @bot.event()
 async def event_message(ctx):
     # 'Runs every time a message is sent in chat.'
+    print(ctx.author.name)
+    print(ctx.author.is_mod)
     print(ctx.content)
 
     # make sure the bot ignores itself and the streamer
@@ -35,20 +38,47 @@ async def event_message(ctx):
 
     await bot.handle_commands(ctx)
 
-    #Roleta Russa do funk
+    # Bot agression check  
+    if bool(re.search('bot.*burro', ctx.content.lower())):
+        await ctx.channel.send('BibleThump')
+        return
+
+    # Soneca Check
+    if bool(re.search(' soneca ', ctx.content.lower())):
+        await ctx.channel.send('PogChamp Oda mentioned PogChamp')
+        return
+
+    # Enzo Check
+    if bool(re.search(' soneca ', ctx.content.lower())):
+        await ctx.channel.send('PogChamp Enzo mentioned PogChamp')
+        return
+
+    # Adair Check
+    if bool(re.search(' oda | adair ', ctx.content.lower())):
+        await ctx.channel.send('Meu deus!!! Falou algo pesado aí, aqui chegou a cair a conexão com o chat! LUL')
+        return
+
+    # Salve Check
+    if 'salve' in ctx.content.lower():
+        await ctx.channel.send(f"Salve, @{ctx.author.name}!")
+        return
+    
+    #Roleta Russa
     a=0
+    # Mod micro agression
     if randint(1, 5) == 3 and ctx.author.is_mod:
         a=1
         await ctx.channel.send(f'Mod é broxa @{ctx.author.name}')
+    # Boludo micro agression
     if (randint(1, 30) == 15) and a == 0:
         a=1
         await ctx.channel.send('15 anos no Brasil e não fala um portugues sem sotaque')
+    # Viva o funk
     if (randint(1, 10) == 5) and a == 0:
         await ctx.channel.send('Bota um funk ai pra nois Boludo')
-    print(ctx.author.is_mod)
     a=0
-    if 'salve' in ctx.content.lower():
-        await ctx.channel.send(f"Salve, @{ctx.author.name}!")
+    #End Roleta Russa
+
 
 
 @bot.command(name='shrek')
